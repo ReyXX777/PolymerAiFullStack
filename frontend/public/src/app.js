91d2aa1735ab6b9929793680635618571aa17134
ReyXX777 <cse2021100@rcciit.org.in>
2024-10-27 01:27:39 +0530

    Update app.js
    changes/

--- frontend/public/src/app.js
+++ frontend/public/src/app.js
@@ -1,3 +1,5 @@
+// PolymerAiFullStack/frontend/public/src/app.js
+
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
