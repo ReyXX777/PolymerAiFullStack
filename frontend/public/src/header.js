a8c5f5faa705d2ef6b573e9dd61913895d27d873
ReyXX777 <cse2021100@rcciit.org.in>
2024-10-27 01:28:03 +0530

    Update header.js
    changes/

--- frontend/public/src/header.js
+++ frontend/public/src/header.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// PolymerAiFullStack/frontend/public/src/components/Header.js
+
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">PolymerAI</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
+      <div className="container">
+        <Link to="/" className="navbar-brand">PolymerAI</Link>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
+              <Link to="/" className="nav-link">Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
+              <Link to="/about" className="nav-link">About</Link>
             </li>
           </ul>
         </div>
