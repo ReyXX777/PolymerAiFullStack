9c59b4dc3d5e9c4e0b40bb5be81d7a5ecba8ba70
ReyXX777 <cse2021100@rcciit.org.in>
2025-01-12 23:59:20 +0530

    Update index.js

--- frontend/public/src/index.js
+++ frontend/public/src/index.js
@@ -1,1 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+// PolymerAiFullStack/frontend/public/src/index.js
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import ThemeProvider from "./components/ThemeProvider";
+import ErrorBoundary from "./components/ErrorBoundary";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <ErrorBoundary>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </ErrorBoundary>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// Commit message: "Add ThemeProvider and ErrorBoundary to enhance application structure"
